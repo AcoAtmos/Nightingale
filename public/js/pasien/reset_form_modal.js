1319a6c649ed8cda78ad9b35adddba9e2cf60dad
AcoAtmos <utsmanbinali123@gmail.com>
2025-11-21 09:54:11 +0700

    menambah fitur reset form ketika mengklik tambah pasien agar mereset data yang tersimpan ketika mengklik edti

--- public/js/pasien/reset_form_modal.js
+++ public/js/pasien/reset_form_modal.js
@@ -1,12 +1,13 @@
 
 function reset_form(){
-    document.querySelector('div.form_pasien_baru .nama_pasien').value=nama_pasien;
-	document.querySelector('div.form_pasien_baru .no_telp_pasien').value=no_telp_pasien.trim();
-    document.querySelector('div.form_pasien_baru .tanggal_lahir_pasien').value=tanggal_lahir_pasien.trim();
-	document.querySelector('div.form_pasien_baru .alamat_pasien').value=alamat_pasien.trim();
+    document.querySelector('div.form_pasien_baru .nama_pasien').value='';
+	document.querySelector('div.form_pasien_baru .no_telp_pasien').value=''
+    document.querySelector('div.form_pasien_baru .tanggal_lahir_pasien').value='';
+	document.querySelector('div.form_pasien_baru .alamat_pasien').value='';
 
 	document.querySelector('div.form_pasien_baru .pasien_id').value='';
 	document.querySelectorAll('div.form_pasien_baru .jenis_kelamin').forEach(function(el){
 			el.removeAttribute('checked');
 		});
 }
+window.reset_form = reset_form;
--- public/master_pasien.php
+++ public/master_pasien.php
@@ -574,7 +574,7 @@
                 </button>
               </div>
               
-              <button @click="openModal" reset_form()  class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+              <button @click="openModal" onclick="reset_form()"  class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                 <i class="fa-solid fa-plus"><path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" fill-rule="evenodd"></path></i>
                   
                   <span>Tambah pasien</span>
